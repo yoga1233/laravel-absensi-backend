0aebad58a78e4d610c6b7ee4206270dc5e6a6068
yoga_cb <yoga.cncy11@gmail.com>
2024-04-25 15:50:14 +0700

    company dashboard and api company

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Company;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -20,6 +21,18 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@gmail.com',
             'password' => Hash::make('password'),
+        ]);
+
+        //data dummy for company
+        Company::create([
+            'name' => 'PT. Segar Buana Mandiri',
+            'email' => 'cs@segarbuanamandiri.com',
+            'address' => 'Jl. Cempaka Putih No. 1',
+            'latitude' => '-6.200000',
+            'longitude' => '106.816666',
+            'radius_km' => '0.5',
+            'time_in' => '08:00',
+            'time_out' => '17:00',
         ]);
     }
 }
